02b1a64208c1881dd518c754908f77168b3b972a
Edwin Jones <edwinj85@gmail.com>
2014-12-15 22:03:03 +0000

    Initial commit of my binary file hash checker tool.
    Initial commit of my binary file hash checker tool.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ezHashCheck
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.FindFileButton = new System.Windows.Forms.Button();
+            this.HashMethodsComboBox = new System.Windows.Forms.ComboBox();
+            this.HashStringTextBox = new System.Windows.Forms.TextBox();
+            this.FileToHashTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CheckHashButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FindFileButton
+            // 
+            this.FindFileButton.Location = new System.Drawing.Point(305, 144);
+            this.FindFileButton.Name = "FindFileButton";
+            this.FindFileButton.Size = new System.Drawing.Size(75, 23);
+            this.FindFileButton.TabIndex = 0;
+            this.FindFileButton.Text = "...";
+            this.FindFileButton.UseVisualStyleBackColor = true;
+            this.FindFileButton.Click += new System.EventHandler(this.FindFileButton_Click);
+            // 
+            // HashMethodsComboBox
+            // 
+            this.HashMethodsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.HashMethodsComboBox.FormattingEnabled = true;
+            this.HashMethodsComboBox.Location = new System.Drawing.Point(12, 46);
+            this.HashMethodsComboBox.Name = "HashMethodsComboBox";
+            this.HashMethodsComboBox.Size = new System.Drawing.Size(121, 21);
+            this.HashMethodsComboBox.TabIndex = 1;
+            // 
+            // HashStringTextBox
+            // 
+            this.HashStringTextBox.Location = new System.Drawing.Point(12, 96);
+            this.HashStringTextBox.Name = "HashStringTextBox";
+            this.HashStringTextBox.Size = new System.Drawing.Size(287, 20);
+            this.HashStringTextBox.TabIndex = 2;
+            // 
+            // FileToHashTextBox
+            // 
+            this.FileToHashTextBox.Location = new System.Drawing.Point(12, 144);
+            this.FileToHashTextBox.Name = "FileToHashTextBox";
+            this.FileToHashTextBox.Size = new System.Drawing.Size(287, 20);
+            this.FileToHashTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Hash String:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Hash Method:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "File:";
+            // 
+            // CheckHashButton
+            // 
+            this.CheckHashButton.Location = new System.Drawing.Point(62, 210);
+            this.CheckHashButton.Name = "CheckHashButton";
+            this.CheckHashButton.Size = new System.Drawing.Size(274, 39);
+            this.CheckHashButton.TabIndex = 7;
+            this.CheckHashButton.Text = "Check Hash";
+            this.CheckHashButton.UseVisualStyleBackColor = true;
+            this.CheckHashButton.Click += new System.EventHandler(this.CheckHashButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 261);
+            this.Controls.Add(this.CheckHashButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FileToHashTextBox);
+            this.Controls.Add(this.HashStringTextBox);
+            this.Controls.Add(this.HashMethodsComboBox);
+            this.Controls.Add(this.FindFileButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "ezHashCheck";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button FindFileButton;
+        private System.Windows.Forms.ComboBox HashMethodsComboBox;
+        private System.Windows.Forms.TextBox HashStringTextBox;
+        private System.Windows.Forms.TextBox FileToHashTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button CheckHashButton;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace ezHashCheck
+{
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// This enumation lists all the hashing methods this program supports.
+        /// </summary>
+        enum HashMethods
+        {
+            SHA1,
+            MD5
+        }
+
+        /// <summary>
+        /// This string should be used as the all title of all message windows shown to the user.
+        /// </summary>
+        const String MessageTitle = "ezHashCheck";
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            //this.Icon = ezHashCheck.Properties.Resources.Icon;
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            //Add selectable hash methods in the drop down.
+            HashMethodsComboBox.Items.Add(HashMethods.SHA1.ToString());
+            HashMethodsComboBox.Items.Add(HashMethods.MD5.ToString());
+
+            //give the drop down a default value.
+            HashMethodsComboBox.SelectedIndex = 0;
+        }
+
+        /// <summary>
+        /// This method runs when the user presses the 'Check Hash' button.
+        /// </summary>
+        private void CheckHashButton_Click(object sender, EventArgs e)
+        {
+            //local vars
+            Boolean hashMatch = false;
+            String filePath = FileToHashTextBox.Text;
+            String hashString = HashStringTextBox.Text;
+
+
+            //check user input.
+            if (String.IsNullOrEmpty(filePath) || String.IsNullOrEmpty(hashString))
+            {
+                MessageBox.Show("Make sure you have entered a hash string AND chosen a file to check the hash of!", MessageTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if(!OnlyHexInString(hashString))
+            {
+                MessageBox.Show("Hash string must be hexadecimal!", MessageTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            else
+            {
+                
+                byte[] newHash = null;
+                HashAlgorithm hashAlgorithm = null;
+
+                String selectedHashMethod = HashMethodsComboBox.SelectedItem.ToString();
+
+                if (selectedHashMethod == HashMethods.SHA1.ToString())
+                {
+                    hashAlgorithm = SHA1.Create();
+                }
+                else if (selectedHashMethod == HashMethods.MD5.ToString())
+                {
+                    hashAlgorithm = MD5.Create();
+                }
+
+                if (hashAlgorithm != null)
+                {
+                    byte[] oldHash = StringToByteArray(hashString);
+
+                    using (var stream = File.OpenRead(filePath))
+                    {
+                        newHash = hashAlgorithm.ComputeHash(stream);
+                    }
+
+                    if (oldHash.SequenceEqual(newHash))
+                    {
+                        hashMatch = true;
+                    }
+
+                    hashAlgorithm.Dispose();
+                }
+            }
+
+            //tell user if the hash matches or not.
+            if (hashMatch)
+            {
+                MessageBox.Show("Hash matches the chosen file.", MessageTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Hash does not match the chosen file!", MessageTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// This method runs when the user clicks the 'Find File' button (...) - uses an OpenFileDialog.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FindFileButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    FileToHashTextBox.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        /// <summary>
+        /// This function takes a string and returns it as a byte array. The string must have an even number of chars!
+        /// </summary>
+        private Byte[] StringToByteArray(String hex)
+        {
+            int NumberChars = hex.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+            {
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            }
+
+            return bytes;
+        }
+
+        /// <summary>
+        /// This function checks to see if a string is hex. The string must have an even number of chars!
+        /// </summary>
+        private Boolean OnlyHexInString(String test)
+        {
+            return test.Count() % 2 == 0 && System.Text.RegularExpressions.Regex.IsMatch(test, @"\A\b[0-9a-fA-F]+\b\Z");
+        }
+    }
+}
